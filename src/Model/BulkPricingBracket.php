bc51f05109df369b127d2eac88c213342388f592
Makreig <mark@i-lateral.com>
2020-04-16 12:30:32 +0100

    added tax awareness

--- src/Model/BulkPricingBracket.php
+++ src/Model/BulkPricingBracket.php
@@ -3,6 +3,7 @@
 namespace SilverCommerce\BulkPricing\Model;
 
 use SilverStripe\ORM\DataObject;
+use SilverStripe\ORM\FieldType\DBCurrency;
 use SilverCommerce\TaxAdmin\Helpers\MathsHelper;
 use SilverCommerce\BulkPricing\Model\BulkPricingGroup;
 use SilverCommerce\CatalogueAdmin\Model\CatalogueProduct;
@@ -28,4 +29,26 @@ class BulkPricingBracket extends DataObject
     private static $default_sort = [
         'Quantity' => 'ASC'
     ];
+
+    /**
+     * Get the Total price and tax
+     *
+     * @return float
+     */
+    public function getPriceAndTax()
+    {
+        $notax = $this->Price;
+        if ($this->ProductID) {
+            $product = $this->Product();
+            $percent = $product->getTaxPercentage();
+
+            // Round using default rounding defined on MathsHelper
+            $tax = ($notax / 100) * $percent;
+            $price = $notax + $tax;
+
+            return DBCurrency::create()->setValue($price);
+        }
+
+        return $notax;
+    }
 }
