0081f28a141db0c0d2f2fb7b3026acaf0241bf81
Morven Lewis-Everley <morven@i-lateral.com>
2020-04-29 18:39:26 +0100

    Add ability to select grouped pricing and add brackets using uinline editing

--- src/Model/BulkPricingGroup.php
+++ src/Model/BulkPricingGroup.php
@@ -3,10 +3,17 @@
 namespace SilverCommerce\BulkPricing\Model;
 
 use SilverStripe\ORM\DataObject;
+use SilverStripe\Forms\GridField\GridFieldConfig;
+use SilverStripe\Forms\GridField\GridFieldButtonRow;
+use Symbiote\GridFieldExtensions\GridFieldTitleHeader;
+use SilverStripe\Forms\GridField\GridFieldDeleteAction;
 use SilverCommerce\BulkPricing\Model\BulkPricingBracket;
+use SilverStripe\Forms\GridField\GridFieldToolbarHeader;
 use SilverCommerce\CatalogueAdmin\Model\CatalogueProduct;
 use SilverCommerce\CatalogueAdmin\Model\CatalogueCategory;
+use Symbiote\GridFieldExtensions\GridFieldEditableColumns;
 use SilverCommerce\BulkPricing\Model\GroupBulkPricingBracket;
+use Symbiote\GridFieldExtensions\GridFieldAddNewInlineButton;
 
 /**
  * @method CatalogueCategory Category
@@ -17,7 +24,8 @@ class BulkPricingGroup extends DataObject
     private static $table_name = 'BulkPricingGroup';
 
     private static $db = [
-        'Title' => 'Varchar'
+        'Title' => 'Varchar',
+        'GroupedPricing' => 'Boolean'
     ];
     
     private static $has_one = [
@@ -28,6 +36,10 @@ class BulkPricingGroup extends DataObject
         'Products' => CatalogueProduct::class,
         'PricingBrackets' => BulkPricingBracket::class,
         'Brackets' => GroupBulkPricingBracket::class // Legacy, soon to be removed
+    ];
+
+    private static $field_labels = [
+        'GroupedPricing' => 'Apply Pricing To Any Combination Of Products In Group?'
     ];
 
     /**
@@ -45,5 +57,30 @@ class BulkPricingGroup extends DataObject
         $products = CatalogueProduct::get()->filter("ID", $ids);
 
         return $products;
+    }
+
+    public function getCMSFields()
+    {
+        $self = $this;
+
+        $this->beforeUpdateCMSFields(
+            function ($fields) use ($self) {
+                $fields->removeByName('Brackets');
+                $pricing_field = $fields->dataFieldByName('PricingBrackets');
+
+                if (!empty($pricing_field)) {
+                    $config = GridFieldConfig::create()
+                        ->addComponent(new GridFieldButtonRow('before'))
+                        ->addComponent(new GridFieldToolbarHeader())
+                        ->addComponent(new GridFieldTitleHeader())
+                        ->addComponent(new GridFieldEditableColumns())
+                        ->addComponent(new GridFieldDeleteAction())
+                        ->addComponent(new GridFieldAddNewInlineButton());
+                    $pricing_field->setConfig($config);
+                }
+            }
+        );
+
+        return parent::getCMSFields();
     }
 }
