3709d4ccea189d2d6fc1778a5fdd9140877f9c1f
Morven Lewis-Everley <morven@i-lateral.com>
2020-04-30 15:06:13 +0100

    Add Taxable support for bulk pricing bracket

--- src/Model/BulkPricingBracket.php
+++ src/Model/BulkPricingBracket.php
@@ -2,10 +2,13 @@
 
 namespace SilverCommerce\BulkPricing\Model;
 
+use SilverStripe\i18n\i18n;
 use SilverStripe\ORM\DataObject;
 use SilverStripe\Control\HTTPRequest;
 use SilverStripe\Core\Injector\Injector;
-use SilverStripe\ORM\FieldType\DBCurrency;
+use SilverCommerce\TaxAdmin\Model\TaxRate;
+use SilverCommerce\TaxAdmin\Traits\Taxable;
+use SilverCommerce\TaxAdmin\Interfaces\TaxableProvider;
 use SilverCommerce\CatalogueAdmin\Model\CatalogueProduct;
 use SilverCommerce\BulkPricing\Tasks\MigrateLegacyBracketsTask;
 
@@ -13,8 +16,10 @@ use SilverCommerce\BulkPricing\Tasks\MigrateLegacyBracketsTask;
  * @method BulkPricingGroup Group
  * @method CatalogueProduct Product
  */
-class BulkPricingBracket extends DataObject
+class BulkPricingBracket extends DataObject implements TaxableProvider
 {
+    use Taxable;
+
     private static $table_name = 'BulkPricingBracket';
 
     /**
@@ -26,6 +31,13 @@ class BulkPricingBracket extends DataObject
      * @config
      */
     private static $allow_negative = false;
+
+    /**
+     * Temporary product that can be set to calculate nice prices (in template)
+     *
+     * @var null|CatalogueProduct
+     */
+    protected $temp_product = null;
 
     private static $db = [
         'Quantity' => 'Int', // Legacy, will be removed
@@ -68,6 +80,103 @@ class BulkPricingBracket extends DataObject
     }
 
     /**
+     * Return a possible product for this bracket
+     *
+     * @return null|CatalogueProduct
+     */
+    public function getPossibleProduct()
+    {
+        $product = null;
+
+        if ($this->Product()->exists()) {
+            $product = $this->Product();
+        }
+
+        if (empty($product)) {
+            $product = $this->getTempProduct();
+        }
+
+        return $product;
+    }
+
+    /**
+     * Return price suitable for Taxable
+     *
+     * @return float
+     */
+    public function getBasePrice()
+    {
+        $product = $this->getPossibleProduct();
+
+        if (!empty($product)) {
+            return $this->modifyPrice($product->getBasePrice());
+        }
+
+        return (float) $this->Price;
+    }
+
+    /**
+     * Return a Tax Percentage for the current product (if available)
+     *
+     * @return \SilverCommerce\TaxAdmin\Model\TaxRate
+     */
+    public function getTaxRate()
+    {
+        $product = $this->getPossibleProduct();
+
+        if (!empty($product)) {
+            return $product->getTaxRate();
+        }
+
+        $rate = TaxRate::create();
+        $rate->ID = -1;
+
+        return $rate;
+    }
+
+    /**
+     * Provide a string based locale for the current object (EG 'en_GB')
+     *
+     * @return string
+     */
+    public function getLocale()
+    {
+        return i18n::get_locale();
+    }
+
+    /**
+     * Should we automatically show the price including TAX?
+     *
+     * @return bool
+     */
+    public function getShowPriceWithTax()
+    {
+        $product = $this->getPossibleProduct();
+
+        if (!empty($product)) {
+            return $product->getShowPriceWithTax();
+        }
+
+        return false;
+    }
+
+    /**
+     * Should we add a "Tax String" (EG "Includes VAT") to the rendered currency?
+     *
+     * @return bool|null
+     */
+    public function getShowTaxString()
+    {
+        $product = $this->getPossibleProduct();
+
+        if (!empty($product)) {
+            return $product->getShowTaxString();
+        }
+
+        return false;
+    }
+
+    /**
      * Modify a provided price based on the current bracket settings
      *
      * @param float $price
@@ -87,24 +196,21 @@ class BulkPricingBracket extends DataObject
     }
 
     /**
-     * Get the Total price and tax
-     *
-     * @return float
+     * @return  null|CatalogueProduct
      */
-    public function getPriceAndTax()
+    public function getTempProduct()
     {
-        $notax = $this->Price;
-        if ($this->ProductID) {
-            $product = $this->Product();
-            $percent = $product->getTaxPercentage();
+        return $this->temp_product;
+    }
 
-            // Round using default rounding defined on MathsHelper
-            $tax = ($notax / 100) * $percent;
-            $price = $notax + $tax;
-
-            return DBCurrency::create()->setValue($price);
-        }
-
-        return $notax;
+    /**
+     * @param CatalogueProduct $temp_product
+     *
+     * @return self
+     */
+    public function setTempProduct(CatalogueProduct $product)
+    {
+        $this->temp_product = $product;
+        return $this;
     }
 }
